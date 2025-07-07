cc724f8e6ed3685d694b9059c530d1cb4ef78671
Iqra Mahjabeen <mahjabeenraza0405@gmail.com>
2025-07-07 08:43:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+// Question:01
+
+/*
+let x = 10;
+let y = 5;
+
+let sum = x + y ;
+let difference = x - y ;
+let product = x* y ;
+let quotient = x / y ;
+
+console.log(`SUM of X & Y : ${sum}`);
+console.log(`Difference of X & Y : ${difference}`);
+console.log(`Product of X & Y : ${product}`);
+console.log(`Quotient of X & Y : ${quotient}`);
+
+*/
+
+
+//Question:02
+/*
+let age = +prompt('Enter your age :');
+if(age>=18){
+    alert("you are an adult !");
+}
+else{
+    alert("you are a minor:");
+}
+*/
+
+//Question:03
+/*
+let fruits = ["APPLE","BANANA","CHERRY"];
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+*/
+
+//Question:04
+/*
+const firstname = "Iqra";
+const lastname = "Mahjabeen";
+const fullname = firstname + " " + lastname;
+alert("Hello! " + fullname);
+
+*/
+
+
+//Question:05
+/*
+const isAdmin = true;
+const isLoggedIn = true;
+
+if (isLoggedIn) {
+  if (isAdmin) {
+    console.log("Welcome, admin!");
+  }
+}
+  
+output not show
+const isAdmin = true;
+const isLoggedIn = false;
+
+if (isLoggedIn) {
+  if (isAdmin) {
+    console.log("Welcome, admin!");
+  }
+}
+
+*/
